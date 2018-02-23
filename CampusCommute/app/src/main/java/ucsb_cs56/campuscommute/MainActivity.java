040dad3c7cd7ff4eaf0c818acb423555c158bd6b
Ajit Vijayakumar <ajitvijayakumar@umail.ucsb.edu>
2018-02-22 17:39:35 -0800

    initial commit

---
+++ CampusCommute/app/src/main/java/ucsb_cs56/campuscommute/MainActivity.java
@@ -0,0 +1,13 @@
+package ucsb_cs56.campuscommute;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
