fa5e1f3b5fddacc287d98521cb00941ef82a2a70
Ajit Vijayakumar <ajitvijayakumar@umail.ucsb.edu>
2018-02-28 00:22:28 -0800

    map view 1

--- CampusCommute/app/src/main/java/ucsb_cs56/campuscommute/MainActivity.java
+++ CampusCommute/app/src/main/java/ucsb_cs56/campuscommute/MainActivity.java
@@ -1,17 +1,79 @@
 package ucsb_cs56.campuscommute;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private static final String TAG = "MainActivity";
+
+    private static final int ERROR_DIALOG_REQUEST = 9011;
+
+    GoogleMap mGoogleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView = findViewById(R.id.textview);
-        textView.setText("Hey bub");
+        if (isServicesOK()) init();
     }
+    private void init() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    public boolean isServicesOK() {
+        Log.d(TAG, "isServicesOK: checking google services version");
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+        if(available == ConnectionResult.SUCCESS) {
+            // user doesn't need to update version and can make map requests
+            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            return true;
+        }
+        else if( GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
+            Log.d(TAG, "isServicesOK: Fixable error occured");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+            }
+         else {
+                Toast.makeText(this, "You can't make map requests", Toast.LENGTH_SHORT).show();
+            }
+            return false;
+        }
+
+    public void onMapReady(GoogleMap googleMap) {
+        //mGoogleMap = googleMap;
+        //goToLocation(39.008224, -76.8984527);
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions().position(sydney)
+                .title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,10));
+    }
+
+    // private void goToLocation(double lat, double lng) {
+    //LatLng ll =  new LatLng(lat, lng);
+      //  CameraUpdate update = CameraUpdateFactory.newLatLng(ll);
+        //mGoogleMap.moveCamera(update);
+
+
+    //}
 }
