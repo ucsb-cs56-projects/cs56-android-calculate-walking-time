1588caaa3ed9626bbd158040817156743da56f99
Ajit Vijayakumar <ajitvijayakumar@umail.ucsb.edu>
2018-03-08 16:57:09 -0800

    commit w/o commented code

--- CampusCommute/app/src/main/java/ucsb_cs56/campuscommute/MainActivity.java
+++ CampusCommute/app/src/main/java/ucsb_cs56/campuscommute/MainActivity.java
@@ -2,6 +2,12 @@ package ucsb_cs56.campuscommute;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,6 +18,12 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.Api;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -21,13 +33,18 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private static final String TAG = "MainActivity";
 
     private static final int ERROR_DIALOG_REQUEST = 9011;
 
-    GoogleMap mGoogleMap;
+    private GoogleMap mGoogleMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +52,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         setContentView(R.layout.activity_main);
 
         if (isServicesOK()) init();
+
     }
+
     private void init() {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -44,36 +63,27 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public boolean isServicesOK() {
         Log.d(TAG, "isServicesOK: checking google services version");
         int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
-        if(available == ConnectionResult.SUCCESS) {
+        if (available == ConnectionResult.SUCCESS) {
             // user doesn't need to update version and can make map requests
             Log.d(TAG, "isServicesOK: Google Play Services is working");
             return true;
-        }
-        else if( GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
+        } else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
             Log.d(TAG, "isServicesOK: Fixable error occured");
             Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROR_DIALOG_REQUEST);
             dialog.show();
-            }
-         else {
-                Toast.makeText(this, "You can't make map requests", Toast.LENGTH_SHORT).show();
-            }
-            return false;
+        } else {
+            Toast.makeText(this, "You can't make map requests", Toast.LENGTH_SHORT).show();
         }
+        return false;
+    }
 
     public void onMapReady(GoogleMap googleMap) {
-        //mGoogleMap = googleMap;
-        //goToLocation(39.008224, -76.8984527);
         LatLng sydney = new LatLng(-33.852, 151.211);
         googleMap.addMarker(new MarkerOptions().position(sydney)
                 .title("Marker in Sydney"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,10));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15));
+
+
     }
 
-    // private void goToLocation(double lat, double lng) {
-    //LatLng ll =  new LatLng(lat, lng);
-      //  CameraUpdate update = CameraUpdateFactory.newLatLng(ll);
-        //mGoogleMap.moveCamera(update);
-
-
-    //}
 }
